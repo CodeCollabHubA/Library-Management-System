38827c1c0e09e400286a3bce036779c723e21596
Mr-Som3a <mrsom3a8@gmail.com>
2024-04-24 17:59:29 +0400

    style pages

--- Frontend/src/components/SideBar/sidebar.jsx
+++ Frontend/src/components/SideBar/sidebar.jsx
@@ -1,34 +1,36 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGauge, faInbox, faSwatchbook, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = ({ sideBarPosition }) => {
     return (
         <aside id="logo-sidebar" className={`md:w-48 lg:w-64 bottom-0 left-0 z-40 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700 ${sideBarPosition}`} aria-label="Sidebar">
             <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-                <ul className="space-y-2 font-medium">
+                <ul className="space-y-2  mt-6 font-medium">
                     <li>
-                        <NavLink to="/dashboard" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <img className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="/src/components/icons/dashboard.png" />
-                            <span className="ms-3">Dashboard</span>
+                        <NavLink to="/dashboard" className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
+                            <FontAwesomeIcon  fontSize={'1.4rem'} icon={faGauge} />
+                            <span className=" text-lg ms-3">Dashboard</span>
                         </NavLink>
                     </li>
 
                     <li>
                         <NavLink to="/dashboard/inbox" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <img className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="/src/components/icons/inbox.png" />
-                            <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
+                            <FontAwesomeIcon fontSize={'1.4rem'} icon={faInbox} />
+                            <span className="flex-1 text-lg ms-3 whitespace-nowrap">Inbox</span>
                             <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/users" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <img className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="/src/components/icons/users.png" />
-                            <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
+                        <FontAwesomeIcon fontSize={'1.4rem'} icon={faUserGroup} />
+                            <span className="flex-1 text-lg ms-3 whitespace-nowrap">Users</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/books" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <img className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="/src/components/icons/books.png" />
-                            <span className="flex-1 ms-3 whitespace-nowrap">Books</span>
+                            <FontAwesomeIcon fontSize={'1.4rem'} icon={faSwatchbook} />
+                            <span className="flex-1 text-lg ms-3 whitespace-nowrap">Books</span>
                         </NavLink>
                     </li>
                     <li>
--- Frontend/src/partials/dashboard/DashboardAvatars.jsx
+++ Frontend/src/partials/dashboard/DashboardAvatars.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import User01 from '../../images/user-36-01.jpg';
-import User02 from '../../images/user-36-02.jpg';
-import User03 from '../../images/user-36-03.jpg';
-import User04 from '../../images/user-36-04.jpg';
+import User01 from '../../assets/images/user-36-01.jpg';
+import User02 from '../../assets/images/user-36-02.jpg';
+import User03 from '../../assets/images/user-36-03.jpg';
+import User04 from '../../assets/images/user-36-04.jpg';
 
 function DashboardAvatars() {
   return (
