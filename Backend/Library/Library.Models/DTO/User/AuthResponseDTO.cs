59b68dabd8fbde3cbf97368b64127d0c66f747a3
AhmedYassin <dev.ahmedyassin@gmail.com>
2024-05-05 09:38:49 +0400

    feat(dto): add new dto for auth responses

---
+++ Backend/Library/Library.Models/DTO/User/AuthResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Library.Models.DTO
+{
+    public class AuthResponseDTO
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public Role UserRole { get; set; }
+        public string? ImageUrl { get; set; }
+        public string AccessToken { get; set; }
+
+    }
+}
