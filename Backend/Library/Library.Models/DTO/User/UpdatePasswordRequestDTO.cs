e0616c97a227d9bf405aee5986d14edda087dd85
AhmedYassin <dev.ahmedyassin@gmail.com>
2024-05-13 10:17:45 +0400

    feat(dto): add new dto for password update request

---
+++ Backend/Library/Library.Models/DTO/User/UpdatePasswordRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Library.Models.DTO
+{
+    public class UpdatePasswordRequestDTO
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
