ba589e6f49ef930df6055f7f02bf153ffce7f484
Mr-Som3a <mrsom3a8@gmail.com>
2024-05-13 21:59:43 +0400

    feat(redesign):🎨 add reject button to the request detail inside inbox page +  📌repositionack butttton

--- Frontend/src/layout/inbox/requestDetail.jsx
+++ Frontend/src/layout/inbox/requestDetail.jsx
@@ -1,5 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../../components/common/buttons/_button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const RequestDetail = () => {
     const location = useLocation();
@@ -16,14 +18,14 @@ const RequestDetail = () => {
     return (
         <>
             <div className="bg-white mx-auto my-6 p-10 text-center min-h-[20rem] w-[95%] rounded-md">
-            
+                <Button onClick={handleBack} className={'absolute flex items-center justify-evenly w-20 bg-teal-950 hover:text-teal-950 hover:border-teal-950 '}> <FontAwesomeIcon icon={faArrowLeft} /> Back</Button>
                 <h1 className="text-3xl mb-8">Request Details</h1>
                 <h3 className="text-xl my-4">{name}</h3>
                 <p className="my-4">{subject}</p>
                 <p className="my-4">{date}</p>
-                <div className="mx-auto mt-14 flex justify-around">
-                    <Button onClick={handleConfirmBorrowing} type={'primary'} className={"w-40"}>Confirm Borrowing</Button>
-                    <Button onClick={handleBack} className={"w-40 "}>Back</Button>
+                <div className="mx-auto mt-14 flex justify-around ">
+                    <Button onClick={handleConfirmBorrowing}  className={"w-40 bg-teal-800  hover:text-teal-800 hover:border-teal-800"}>Approve</Button>
+                    <Button onClick={handleConfirmBorrowing} className={"w-40 bg-rose-700 hover:text-rose-700 hover:border-rose-700"}>Reject</Button>
                 </div>
             </div>
         </>
