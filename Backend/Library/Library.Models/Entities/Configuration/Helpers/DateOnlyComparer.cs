6b578d4867b913b13c9241d5925cec35c08cb792
AhmedYassin <dev.ahmedyassin@gmail.com>
2024-06-03 11:53:58 +0400

    feat(domain_entities): add DateOnly converter and comparer

---
+++ Backend/Library/Library.Models/Entities/Configuration/Helpers/DateOnlyComparer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Library.Models.Entities.Configuration.Helpers
+{
+    public class DateOnlyComparer : ValueComparer<DateOnly>
+    {
+        public DateOnlyComparer() : base(
+            (d1, d2) => d1.DayNumber == d2.DayNumber,
+            d => d.GetHashCode())
+        {
+        }
+    }
+}
